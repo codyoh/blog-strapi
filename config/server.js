9d84fdc885adaf0ac51154be1af7d66e7715be5b
maker <codyoferrall@gmail.com>
2021-11-01 19:57:19 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b0fba53913871d214467678988e062ed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'baff2b6b-6e3b-4a62-80f2-984c28a6d999'
+};
